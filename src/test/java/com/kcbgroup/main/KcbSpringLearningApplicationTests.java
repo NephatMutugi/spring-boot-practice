02a11e318dc1b9530a2281360ca7d8a2179fe50f
KEN20960 <muchirinephat5@gmail.com>
2022-05-27 09:45:46 +0300

    Docker

--- src/test/java/com/kcbgroup/main/KcbSpringLearningApplicationTests.java
+++ src/test/java/com/kcbgroup/main/KcbSpringLearningApplicationTests.java
@@ -1,13 +1,9 @@
 package com.kcbgroup.main;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KcbSpringLearningApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
